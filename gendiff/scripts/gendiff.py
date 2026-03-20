4c5a5cbd64953b4b7cb17c0cfb6d879459a40bfd
Rafael <rafael.panaxov14@gmail.com>
2026-03-20 11:34:11 +0400

    second commit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,15 @@
 import argparse
 
 def main():
-    # Создаем парсер с описанием, которое просит Хекслет
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    
-    # Добавляем аргументы (обязательно в таком порядке)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    
+    parser.add_argument('-f', '--format', help='set format of output')
 
-    # Парсим аргументы (это и выведет справку при флаге -h)
-    args = parser.parse_args()
-    print(args)
+    parser.parse_args()
+
 if __name__ == '__main__':
     main()

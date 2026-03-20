87f8612d92a04b318f2471cfbb5dfcab8ec278a4
Rafael <rafael.panaxov14@gmail.com>
2026-03-20 12:54:57 +0400

    fourth commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/dr-Panakhov/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/dr-Panakhov/python-project-50/actions)
+
+[![asciicast](https://asciinema.org/a/UAUdcS3VpgfBHL4S.svg)](https://asciinema.org/a/UAUdcS3VpgfBHL4S)
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,24 @@
 import json
 
+def format_value(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    return value
+
 def generate_diff(file_path1, file_path2):
     data1 = json.load(open(file_path1))
     data2 = json.load(open(file_path2))
-    
-    print(data1)
-    print(data2)
-    
-    return "Diff will be here soon!"
+    all_keys = sorted(set(data1.keys()) | set(data2.keys()))
+    result = []
+    for key in all_keys:
+        if key not in data2:
+            result.append(f"  - {key}: {format_value(data1[key])}")
+        elif key not in data1:
+            result.append(f"  + {key}: {format_value(data2[key])}")
+        elif data1[key] == data2[key]:
+            result.append(f"    {key}: {format_value(data1[key])}")
+        else:
+            result.append(f"  - {key}: {format_value(data1[key])}")
+            result.append(f"  + {key}: {format_value(data2[key])}")
+
+    return "{\n" + "\n".join(result) + "\n}"
